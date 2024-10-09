8a2347e42da8b60101aa515643b9a604c44f89c2
Mohamed Hassan Mohamed <161417196+Mohassan863@users.noreply.github.com>
2024-10-09 11:34:21 +0300

    Create prayer.jsx

---
+++ src/prayer.jsx
@@ -0,0 +1,12 @@
+const prayer = ({name, time}) => {
+    return (
+   <div  className="prayer">
+
+<p className="name-prayer">{name}</p>
+<p className="time-prayer">{time}</p>
+   </div>
+
+    )
+}
+
+export default prayer;
